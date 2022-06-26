f8eac8dafbbbcd8b7643cb68a3f722215e144c63
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-06-26 16:16:47 +0200

    Added test method to trigger issue report about "private method"

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,8 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Database\NodeCollection;
+use App\Models\Node;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+	/**
+	 * @return NodeCollection<Node>
+	 */
+	public function getSiblings(): NodeCollection
+	{
+		/** @var Node $node */
+		$node = Node::query()->find(42);
+		return $node->siblings()->whereBetween('id', [1,2])->get();
+	}
 }
